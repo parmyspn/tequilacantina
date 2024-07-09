926d0ec4b9423817a0288e88d96de44dc8281c01
parmyspn <parmisprv@gmail.com>
2024-07-09 00:49:06 -0700

    adds saturday

--- app/HomePage.tsx
+++ app/HomePage.tsx
@@ -15,7 +15,7 @@ function HomePage() {
           <p>1933 Lonsdale Avenue, North Vancouver, V7M 2K3</p>
           <div className="hours">
             <p>Monday - Thursday: 11 AM - 10 PM</p>
-            <p>Friday: 11 AM - 11 PM</p>
+            <p>Friday and Saturday: 11 AM - 11 PM</p>
             <p>Sunday: 11 AM - 9 PM</p>
           </div>
           <p className="phone">Phone: 604-770-4433</p>
