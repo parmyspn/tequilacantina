a43f8327f3e38fedb13329f20b47f28b9b821037
parmyspn <parmisprv@gmail.com>
2024-07-09 13:19:03 -0700

    fix the scaling

--- app/BackgroundPhoto.tsx
+++ app/BackgroundPhoto.tsx
@@ -3,7 +3,7 @@ import React from "react";
 function BackgroundPhoto() {
   return (
     <div className="background-cover">
-      <div className="background-cover-image has-parallax lazyloaded"></div>
+      <div className="background-cover-image has-parallax lazyloaded object-scale-down"></div>
       <div className="block-cover__inner-container">
         <p className="has-text-align-center has-large-font-size"></p>
       </div>
